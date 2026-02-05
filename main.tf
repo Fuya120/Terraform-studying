681d6868e36e385adae04d91882d178ad8c90bed
堺　風彌 <fuyahoo1031@gmail.com>
2026-02-05 17:28:17 +0900

    Terraform練習2回目

--- main.tf
+++ main.tf
@@ -13,10 +13,41 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_key_pair" "my_key" {
+  key_name = "tf_key"
+  public_key = file("./terraform-key.pub")
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_myip"
+  description = "allow SSH only myip"
+
+  tags = {
+    Name = "tf-sg"
+  }
+  # インバウンド設定
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}/32"] 
+  }
+
+  # アウトバウンド設定
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # すべてのプロトコル
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-
+  key_name      = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
     Name = var.instance_name
   }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,19 @@
 variable "aws_region" {
   description = "AWSのリージョン"
   type        = string
-  default     = "ap-northeast-1"
 }
 
 variable "instance_type" {
   description = "EC2のインスタンスタイプ"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "instance_name" {
   description = "サーバーの名前"
   type        = string
-  default     = "my-server"
+}
+
+variable "my_ip" {
+  description = "マイIP"
+  type        = string
 }
